c90770c2040eca11b125fdb0b4790a8df8e71239
MSVSURFPRO\marbin3dSurfacePro <marbin3d@hotmail.com>
2018-05-10 19:58:03 +1000

    update files integration

--- dbConnect.php
+++
@@ -1,7 +0,0 @@
-<?php
-/*database connection*/
-
-$db="gameClipsDatabase";
-$con=mysqli_connect("localhost","root","",$db);
-
-?>
--- js/myClips.js
+++ js/myClips.js
@@ -20,16 +20,19 @@ console.log(fnameUser);
 
 //load videos available in the platform for general user or active user videos 
 
-function retrieveVideos(emailUsrActive) {   
+function retrieveVideosMyClips(emailUsrActive) {   
     
     var rpReqUrl = "../php/loadVideosMyClips.php";
+    
+    
+    console.log("send data: " + emailUsrActive.replace(/\s/g,'').replace("\n"," "));
 
     $.post(rpReqUrl,
 
         //data
         {
-            //activeUser
-            userEmail: emailUsrActive
+            //activeUser and remove white spaces around the email
+            userEmail: emailUsrActive.replace(/\s/g,'').replace("\n"," ")
 
         },
 
@@ -38,27 +41,22 @@ function retrieveVideos(emailUsrActive) {
 
             //loop OVER JSON OBJECT
             $.each(dataClipsServerDBReply, function (index, value) {               
-
-                console.log(value.length);
+             console.log(value.length);
                 
                 //retrive each video info
                 for (m in value) {
 
                    // var usrId = value[m].userID;
                     var videoTitle = value[m].title;                   
-                    var sourceVideo = value[m].sourceLinkVideo+"?autoplay=0"; 
+                    var sourceVideo = value[m].sourceLinkVideo+""; 
                     var viewsVideo =  value[m].views;
                     
                     var videoUploadByAuthor =  value[m].videoUploadBy;
                      var dateUploadVideo =  value[m].videoDateUpload;
                      var categoryVideo =  value[m].category;
                     
-                 
-                    
                     var videoId = value[m].videoID;
                     var itemListID = videoId  + "";
-                    
-                    
                     
                  // console.log(value[m].userID);
                     console.log(videoTitle);
@@ -66,7 +64,7 @@ function retrieveVideos(emailUsrActive) {
                     console.log(viewsVideo);
                                      
                     //slice details
-                    //var descriptiont=contDetails.slice(0,100)+"...";
+                    //var subject=contDetails.slice(0,100)+"...";
                     
                      var FirstNameUser=fnameUser;
                
@@ -90,28 +88,21 @@ function retrieveVideos(emailUsrActive) {
                     
                       var dateVideo = dateUploadVideo;
                     
-                      var videoFrame = ' <div class="col-xs-10" class="embed-responsive embed-responsive-16by9">                                <iframe class="embed-responsive-item" src= '+sourceVideo +' width="100%" height="100%"   frameborder="0"  ></iframe> </div>';
+                    /*container definition*/                    
+                      var videoFrame = ' <div class="col-xs-10" class="embed-responsive embed-responsive-16by9">                                <video class="embed-responsive-item" src= '+sourceVideo +' width="100%" height="100%"   frameborder="0" preload="metadata" controls ></video> </div>';
                     
                     /*Thumbnail by default*/
                    var  pictDefault='<img src="images/defaultScreenVideo.png" class="screenDefault" class="img-responsive img-thumbnail" width="150" height="100">';
                     
                     /*Thumbnail by item if saved on DB as png or gif*/
                    //var  pictDefault='<img src='+thumbnailImg+' class="screenDefault" class="img-responsive img-thumbnail" width="150" height="150">';
+                                        
+                    var btnPlayVideo='<button style="margin-right: 5px;" type="button" class="btn btn-info btn-sm glyphicon glyphicon-play-circle" data-toggle="modal" data-target="#' + itemListID + '" title="Game Clips Player"> '+pictDefault+'</button>';   
                     
-                    
-                    
-                    var btnPlayVideo='<button style="margin-right: 5px;" type="button" class="btn btn-info btn-sm glyphicon glyphicon-play-circle" data-toggle="modal" data-target="#' + itemListID + '" title="Game Clips Player"> '+pictDefault+'</button>'
-                    
-                     var btnAddVideoToMyClips='<button type="button" class="btn btn-primary" data-dismiss="modal"   onclick="sendAddToMyClipsDB(' + videoId + ')">Add to My Clips</button>  '
-    
+                    var btnAddVideoToMyClips='<button type="button" class="btn btn-primary" data-dismiss="modal"   onclick="inviteSubscribe(' + videoId + ')">Add to My Clips</button>  ';    
 
                     //FORMAT video Item
-                    var itemVideo = '<li href="#" class="list-group-item text-left"> ' + btnPlayVideo+    '<label class="name">' +  FirstNameUploader  + ' <br>Date video: ' + dateVideo  + '<h6>Category: ' + category + '</h6><br></label><label class="pull-right">    <!-- Modal Play video--><div class="modal fade" id="' + itemListID + '" role="dialog"><div class="modal-dialog">    <!-- Modal content--><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal">&times;</button><h4 font-color="gery"class="modal-title"><strong>' + FirstNameUploader+ '</strong> <span style="color:rgb(0, 160, 70)">Views.</span></h4></div>    <div class="modal-body"><h6>category</h6><blockquote>' + category + '</blockquote><h3>Clip</h3><blockquote>' +videoFrame + '</blockquote><h6>Language</h6><blockquote>' + language + '</blockquote><h6>' + additionalDetails + '</h6></div>     <div class="modal-footer"> <button type="button" class="btn btn-primary" data-dismiss="modal"   onclick="sendNewVideoView(' + videoId + ')">Remove Clip</button> <!-- new Btn --> '+btnAddVideoToMyClips +' <button type="button" class="btn btn-default" data-dismiss="modal">Closing player</button>   </div></div></div></div><a  class="btn btn-danger  btn-sm glyphicon glyphicon-trash" href="#" id="' +videoId + '" onclick="removeVideo(' + videoId + ')" title="Remove Video"></a> </label>    <div class="break"></div></li>' ;
-                    
-                  
-                      
-                     /* var videoIn = document.getElementById(videoId+"");
-                       videoIn.pause();*/
+                    var itemVideo = '<li href="#" class="list-group-item text-left"> ' + btnPlayVideo+    '<label class="name">' +  FirstNameUploader  + ' <br>Date video: ' + dateVideo  + '<h6>Category: ' + category + '</h6><br></label><label class="pull-right">    <!-- Modal Play video--><div class="modal fade" id="' + itemListID + '" role="dialog"><div class="modal-dialog">    <!-- Modal content--><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal">&times;</button><h4 font-color="gery"class="modal-title"><strong>' + FirstNameUploader+ '</strong> <span style="color:rgb(0, 160, 70)">Views.</span></h4></div>    <div class="modal-body"><h6>category</h6><blockquote>' + category + '</blockquote><h3>Clip</h3><blockquote>' +videoFrame + '</blockquote><h6>Language</h6><blockquote>' + language + '</blockquote><h6>' + additionalDetails + '</h6></div>     <div class="modal-footer">    <!-- new Btn --> '+btnAddVideoToMyClips+'<button type="button" class="btn btn-default" data-dismiss="modal">Closing player</button>   </div></div></div></div><a  class="btn btn-danger  btn-sm glyphicon glyphicon-trash" href="#" id="' +videoId + '" onclick="inviteSubscribe(' + videoId + ')" title="Remove Video"></a> </label>    <div class="break"></div></li>' ;
                                         
 
                     $('#mainpanecontentSearch').prepend(itemVideo);
@@ -123,20 +114,32 @@ function retrieveVideos(emailUsrActive) {
 
             });
 
-
         },
         'json');
 
 } //end 
 
 
+ //var emailUser1  = document.getElementById("activeEmail").innerHTML;
+//var emailUser1  =  document.getElementById("activeEmail").innerHTML ;
+
+ //var emailUser1=$('#activeEmail').val();
 
 
 $(document).ready(function () {
     
+   // console.log(typeof emailUser);
     
+   //emailUser="marbin3d@hotmail.com";
+  // emailUser  = document.getElementById('activeEmail').innerHTML;
+    
+    
+
+   retrieveVideosMyClips(JSON.stringify(emailUser));
+    
+   // var emailUser1  = $('#activeEmail').val()+"";
      /*load Videos in myClips */
-     retrieveVideos(emailUser);
+     //retrieveVideos( emailUser1);
     
     
     
